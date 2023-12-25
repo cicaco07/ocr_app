11aac1a27c1bc2f77748e6153f863967c9cae216
Aryo <chiefdevica@gmail.com>
2023-12-25 18:20:14 +0700

    feat: accountpage ui

--- lib/pages/account.dart
+++ lib/pages/account.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ocr_app/pages/signin_screen.dart';
+import 'package:ocr_app/utils/color_utils.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -13,20 +14,58 @@ class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          "Account",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          const CircleAvatar(
+            radius: 70,
+            backgroundImage: NetworkImage(''),
+            backgroundColor: Colors.black12,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('Profile'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
+            onTap: () async {
+              await FirebaseAuth.instance.signOut();
+              // ignore: use_build_context_synchronously
               Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => const SignInScreen(),
                 ),
               );
-            });
-          },
-        ),
+            },
+          ),
+        ],
       ),
     );
   }
